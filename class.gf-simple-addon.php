f89051d42ecc8d9f673fe702de42691a733cbe5b
NikV <techvoltz@gmail.com>
2015-06-14 21:47:32 -0500

    Initial boilerplate commit

---
+++ class.gf-simple-addon.php
@@ -0,0 +1,195 @@
+<?php
+/**
+ * The Main Class for the Gravity Forms Simple Add-on
+ *
+ * @author Nikhil Vimal
+ * @since 1.0
+ */
+
+if (class_exists("GFForms")) {
+
+	// Let's include the add-on framework with the GFForms Class
+	GFForms::include_addon_framework();
+
+	/**
+	 * Class GFSample and MUST extend the GFAddOn Class
+	 *
+	 * The class that will hold most of the code for our add-on
+	 */
+	class GFSample extends GFAddOn {
+
+		// Protected variables for use only in the Class
+		protected $_version = "1.0";
+		protected $_min_gravityforms_version = "1.7.9999";
+		protected $_slug = "simpleaddon";
+		protected $_path = "simpleaddon/simpleaddon.php";
+		protected $_full_path = __FILE__;
+		protected $_url = "http://www.gravityforms.com";
+		protected $_title = "Gravity Forms Simple Add-On";
+		protected $_short_title = "Test Add-On";
+
+
+		/**
+		 * This function is meant to be used when you'll need to use an action or hook or filter.
+		 *
+		 * @since 1.0
+		 */
+		// public function init() {
+
+		//	parent::init();
+
+		// add_action here
+
+
+		// }
+
+		/**
+		 * A simple page that you can use to add extra options to your plugin or present an introduction to your add-on
+		 */
+		public function plugin_page() {
+			?>
+
+			<strong>This is an infomational page. Not a required thing to have, but it's nice, is suppose. ¯\_(ツ)_/¯</strong>
+
+			<?php
+		}
+
+		/**
+		 * All fields registerd here go into the Form Settings tab when creating a new form
+		 *
+		 * @param $form The current form
+		 *
+		 * @return array Array for settings fields for the form
+		 */
+		public function form_settings_fields( $form ) {
+			return array(
+				array(
+					"title"  => "Simple Add-On Settings",
+					'description' => 'This is a description of the purpose of Section 1',
+					"fields" => array(
+						array(
+							"name"    => "textbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "text",
+							"class"   => "small",
+							"required" => true,
+
+						),
+						array(
+							"name"    => "textbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "radio",
+							"class"   => "small",
+							'choices'       => array(
+								array(
+									'name'   => 'choice1',
+									'tooltip'=> 'this is the tooltip for choice1',
+									'label'  => 'this is the label for choice1',
+									'value'  => 'this is the value for choice1'
+								),
+								array(
+									'name'    => 'choice2',
+									'tooltip' => 'this is the tooltip for choice2',
+									'label'   => 'this is the label for choice2',
+								),
+							),
+						),
+						array(
+							"name"    => "samplecheckbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "checkbox",
+							"class"   => "small",
+							'onclick' => 'alert("Right you are! Good Choice")',
+							'choices'       => array(
+								array(
+									'name'   => 'choice1',
+									'tooltip'=> 'this is the tooltip for choice1',
+									'label'  => 'this is the label for choice1',
+									'value'  => 'this is the value for choice1'
+								),
+								array(
+									'name'    => 'choice2',
+									'tooltip' => 'this is the tooltip for choice2',
+									'label'   => 'this is the label for choice2',
+								),
+							),
+						),
+					)
+				)
+			);
+		}
+
+		/**
+		 * All fields here go into the Gravity Form Plugin Settings Page
+		 *
+		 * @return array Settings fields for the plugin settings
+		 */
+		public function plugin_settings_fields() {
+			return array(
+				array(
+					"title"  => "Simple Add-On Settings",
+					'description' => 'This is a description of the purpose of Section 1',
+					"fields" => array(
+						array(
+							"name"    => "textbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "text",
+							"class"   => "small",
+							"required" => true,
+
+						),
+						array(
+							"name"    => "textbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "radio",
+							"class"   => "small",
+							'choices'       => array(
+								array(
+									'name'   => 'choice1',
+									'tooltip'=> 'this is the tooltip for choice1',
+									'label'  => 'this is the label for choice1',
+									'value'  => 'this is the value for choice1'
+								),
+								array(
+									'name'    => 'choice2',
+									'tooltip' => 'this is the tooltip for choice2',
+									'label'   => 'this is the label for choice2',
+								),
+							),
+						),
+						array(
+							"name"    => "samplecheckbox",
+							"tooltip" => "This is the tooltip",
+							"label"   => "This is the label",
+							"type"    => "checkbox",
+							"class"   => "small",
+							'onclick' => 'alert("Right you are! Good Choice")',
+							'choices'       => array(
+								array(
+									'name'   => 'choice1',
+									'tooltip'=> 'this is the tooltip for choice1',
+									'label'  => 'this is the label for choice1',
+									'value'  => 'this is the value for choice1'
+								),
+								array(
+									'name'    => 'choice2',
+									'tooltip' => 'this is the tooltip for choice2',
+									'label'   => 'this is the label for choice2',
+								),
+							),
+						),
+					)
+				)
+			);
+		}
+
+	}
+
+	// Let's run our new class!
+	new GFSample();
+}
---
+++ gf-simple-addon.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Gravity Forms Simple Add-On
+Plugin URI: http://www.gravityforms.com
+Description: A simple add-on to demonstrate the use of the Add-On Framework
+Version: 1.0
+Author: Rocketgenius
+Author URI: http://www.rocketgenius.com
+------------------------------------------------------------------------
+Copyright 2015 Rocketgenius Inc.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+// Constant for Version of our Sample Add-on
+define('GF_SAMPLE_VERSION', '2.3.7');
+
+// Fire the class when the form is loaded
+add_action('gform_loaded', array('GF_Test', 'load'), 5);
+
+/**
+ * Class GF_Test is our Bootstrap class, it loads a lot of the important stuff.
+ */
+class GF_Test {
+
+	static function load() {
+		if( ! method_exists('GFForms', 'include_payment_addon_framework') ) {
+			return;
+		}
+
+		// Require our class file
+		require_once('class.gf-simple-addon.php');
+
+		// Let's register our add-on with Gravity Forms
+		GFAddOn::register( 'GFSample' );
+	}
+
+
+}
+
+
+function gf_sample() {
+	return GFSample::get_instance();
+}
